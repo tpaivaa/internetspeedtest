3bd4b9cbcca330779b3af634d891157b2c3ae67f
Tomi Hakkarainen <tpaivaa@gmail.com>
2020-12-06 15:24:10 +0200

    first version no tests yet

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # internetspeedtest
 Test internetspeed and log to influxdb
+
+# requirements
+# Config file where Influx database parameters in python dictionary
+# Just change value to fit your setups
+# config.py 
+```python
+{
+  "host":"x.x.x.x",
+  "port":"8086",
+  "dbuser":"username",
+  "dbuser_password":"password"
+}
+```
+# Use on app.py
+```
+import config as c
+
+dbclient = InfluxDBClient(c.host, c.port, c.user, c.password, c.dbname)
+
+```
+
+
---
+++ app.py
@@ -0,0 +1,37 @@
+import time, speedtest, json, datetime, sys, traceback, os
+from datetime import timedelta
+from influxdb import InfluxDBClient
+import config as c
+
+def exceptionHandleri(e):
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback.print_exc(limit=2, file=sys.stdout)
+    print(repr(traceback.extract_tb(exc_traceback)))
+    print(exc_type, exc_value)
+    print('Ja se Error: ', e)
+
+try:
+  dbclient = InfluxDBClient(c.host, c.port, c.user, c.password, c.dbname)
+  start_time = time.time()
+  receiveTime=datetime.datetime.utcnow()
+  #
+  # Perform computations.
+  #
+  st = speedtest.Speedtest()
+  download = st.download()
+  elapsed_time_secs = time.time() - start_time
+  output =  {"download": str(download) , "executionSpeed" : str(elapsed_time_secs) }
+
+  json_body = [
+    { "measurement": 'netSpeed',
+      "time": receiveTime,
+      "fields": {
+          "value": download
+      }
+    }
+  ]
+  dbclient.write_points(json_body)
+  print (json.dumps(output, indent=4))
+
+except Exception as e:
+  exceptionHandleri(e)
